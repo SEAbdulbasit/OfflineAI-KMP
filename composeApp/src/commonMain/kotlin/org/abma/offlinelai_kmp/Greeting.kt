664cd4fe032c9a271fe8163ba94b6fd148a411e2
Abdul Basit <abdulbasit5361234@gmail.com>
2026-01-20 06:00:23 +0300

    initialize Kotlin Multiplatform project with Android and iOS support

---
+++ composeApp/src/commonMain/kotlin/org/abma/offlinelai_kmp/Greeting.kt
@@ -0,0 +1,9 @@
+package org.abma.offlinelai_kmp
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/org/abma/offlinelai_kmp/MainViewController.kt
@@ -0,0 +1,5 @@
+package org.abma.offlinelai_kmp
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
